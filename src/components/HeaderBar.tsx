a40717423e59a599de5356b75c5367ddcf9c70ce
ReDxDaGer <yashmehrotra45@gmail.com>
2024-04-01 19:56:37 +0530

    Added more components

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,14 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import GradientBGIcon from './GradientBGIcon';
+import ProfilePic from './ProfilePic';
 
-const HeaderBar = () => {
+interface HeaderBarProps {
+  title?: String;
+}
+
+const HeaderBar:React.FC<HeaderBarProps> = ({title}) => {
   return (
-    <View>
-      <Text>HeaderBar</Text>
+    <View style={styles.HeaderContainer}>
+      <GradientBGIcon name='menu' color={COLORS.primaryLightGreyHex} size={FONTSIZE.size_16} />
+      <Text style={styles.HeaderText}>{title}</Text>
+      <ProfilePic />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  HeaderContainer:{
+    padding:SPACING.space_30,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between'
+  },
+  HeaderText:{
+    fontFamily:FONTFAMILY.poppins_semibold,
+    fontSize:FONTSIZE.size_20,
+    color:COLORS.primaryWhiteHex
+  },
+})
 
 export default HeaderBar
