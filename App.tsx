0818c9481034d2682aecdeaa1fb7353dedbb1aa7
ReDxDaGer <yashmehrotra45@gmail.com>
2024-04-01 19:55:37 +0530

    Updated styling

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabNavigator from './src/navigators/TabNavigator';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
+import CustomIcon from './src/components/CustomIcon';
 
 
 const Stack = createNativeStackNavigator();
@@ -14,9 +14,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Tab" component={TabNavigator} options={{animation:'slide_from_bottom'}}></Stack.Screen>
-        <Stack.Screen name="Details" component={DetailsScreen} options={{animation:'slide_from_bottom'}}></Stack.Screen>
-        <Stack.Screen name="Payment" component={PaymentScreen} options={{animation:'slide_from_bottom'}}></Stack.Screen>
+        <Stack.Screen name="Tab" component={TabNavigator} options={{animation:"slide_from_bottom"}}></Stack.Screen>
+        <Stack.Screen name="Details" component={DetailsScreen} options={{animation:"slide_from_bottom"}}></Stack.Screen>
+        <Stack.Screen name="Payment" component={PaymentScreen} options={{animation:"slide_from_bottom"}}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
